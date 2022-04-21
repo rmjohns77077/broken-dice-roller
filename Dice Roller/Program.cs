b644d7d5b65f748f0219009c21ed0f80a8b0265b
rmjohns77077 <Ryanmj7707@outlook.com>
2022-04-20 22:05:26 -0400

    Add project files.

---
+++ Dice Roller/Program.cs
@@ -0,0 +1,65 @@
+﻿public class Program
+{
+    public class diceRoller
+    {
+        //public static int sixSideDice(int num1, int num2, int num3)
+        //{
+        //   int num1= Random.Next
+        //}
+
+
+            static void Main()
+            {
+                Console.WriteLine("How many side dice would you like to roll?");
+                string userInput = Console.ReadLine();
+                int sides = int.Parse(userInput);
+                if (userInput == "6")
+                {
+
+
+
+
+                    Console.WriteLine("Lets get Rollin'!");
+                    Random random = new Random();
+
+                    int num1 = random.Next(1, 7);
+                    int num2 = random.Next(1, 7);
+                    int totalRoll = num1 + num2;
+
+                    if (num1 + num2 == 2)
+                    {
+                        Console.WriteLine("snake eyes" + num1 + "," + num2);
+                    }
+                    if (num1 + num2 == 12)
+                    {
+                        Console.WriteLine("Boxcars!" + num1 + "," + num2);
+                    }
+                    else if (totalRoll == 7 || totalRoll == 11)
+                    {
+                        Console.WriteLine("Winner!!!! " + num1 + "," + num2);
+                    }
+                    else if (num1 + num2 >= 2)
+                    {
+                        Console.WriteLine("Your rolls were " + num1 + " and " + num2);
+                    }
+                }
+
+                else if (userInput != "6")
+                {
+                    Console.WriteLine("Should've choosen 6");
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+        
+
+    }
+}
